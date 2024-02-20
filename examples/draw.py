b461182ceef2467410ea9ed61b3c7fc2e9e36368
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2024-02-20 00:07:06 -0800

    Speed up draw.py to realtime

--- examples/draw.py
+++ examples/draw.py
@@ -1,68 +1,60 @@
-#!/usr/bin/env python3
+import cv2
 import numpy as np
-from tinygrad import Tensor
-from PIL import ImageGrab
+import time
 from serious_mnist import BigConvNet
-import tkinter as tk
+from tinygrad import Tensor
 
-class DrawingApp:
-  def __init__(self, master, model):
-    self.master = master
-    self.model = model
-    self.canvas = tk.Canvas(master, width=400, height=400, bg="black")
-    self.canvas.pack()
+model = BigConvNet()
+model.load("examples/checkpoint968900.safetensors")
+input_canvas_title = "Draw number. Q: Quit. C: Clear"
+output_canvas_title = "Prediction"
+width = 400
+pen_color = (255, 255, 255)
+pen_thickness = 25
 
-    self.label = tk.Label(master, text="Draw a number")
-    self.label.pack()
+def create_blank_canvas(): return np.zeros((width, width, 3), dtype="uint8")
 
-    self.clear_button = tk.Button(master, text="Clear", command=self.clear_canvas)
-    self.clear_button.pack()
+input_canvas = create_blank_canvas()
+output_canvas = create_blank_canvas()
 
-    self.read_button = tk.Button(master, text="Read", command=self.read_canvas)
-    self.read_button.pack()
+last_point = None
 
-    self.previous_x = None
-    self.previous_y = None
+def draw(event, x, y, flags, param):
+  global last_point
+  if event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):
+    if last_point is not None:
+      cv2.line(input_canvas, last_point, (x, y), pen_color, pen_thickness)
+    last_point = (x, y)
+  elif event == cv2.EVENT_LBUTTONUP:
+    last_point = None
 
-    self.canvas.bind("<B1-Motion>", self.paint)
-    self.canvas.bind("<ButtonRelease-1>", self.reset)
+cv2.namedWindow(input_canvas_title)
+cv2.setMouseCallback(input_canvas_title, draw)
+cv2.imshow(input_canvas_title, input_canvas)
+cv2.imshow(output_canvas_title, output_canvas)
+cv2.moveWindow(input_canvas_title, 0, 0)
+cv2.moveWindow(output_canvas_title, width, 0)
 
-  def clear_canvas(self):
-    self.canvas.delete("all")
+last_save_time = time.time()
+last_prediction = None
 
-  def paint(self, event):
-    paint_color = "white"
-    if self.previous_x and self.previous_y:
-        self.canvas.create_line(self.previous_x, self.previous_y, event.x, event.y,
-                                width=16, fill=paint_color, capstyle=tk.ROUND, smooth=tk.TRUE)
-    self.previous_x = event.x
-    self.previous_y = event.y
+try:
+  while True:
+    cv2.imshow(input_canvas_title, input_canvas)
+    cv2.imshow(output_canvas_title, output_canvas)
 
-  def reset(self, event):
-    self.previous_x = None
-    self.previous_y = None
+    if (time.time() - last_save_time < 0.1):
+      image = cv2.resize(input_canvas, (28, 28), interpolation=cv2.INTER_AREA)[:, :, 0]
+      output = model.forward(Tensor(image))
+      prediction = np.argmax(output.numpy(), axis=-1)[0]
+      if (last_prediction != prediction):
+        output_canvas = create_blank_canvas()
+        cv2.putText(output_canvas, str(prediction), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 10, pen_color, pen_thickness)
+      last_prediction = prediction
+      last_save_time = time.time()
 
-  def read_canvas(self):
-    # TODO: Find a GUI library that can handle Retina displays
-    x = self.master.winfo_rootx() + 15
-    y = self.master.winfo_rooty() + 80
-    x1 = x + self.canvas.winfo_width() * 2 - 20
-    y1 = y + self.canvas.winfo_height() * 2 - 20
-
-    # TODO: grab() costs 300ms. Find a GUI library that can give pixels
-    image = ImageGrab.grab().crop((x, y, x1, y1)).convert("L").resize((28, 28))
-    input = Tensor(np.array(image).reshape(-1, 28, 28).astype(np.uint8))
-    print(input.numpy())
-
-    output = model.forward(input)
-    pred = np.argmax(output.numpy(), axis=-1)[0]
-    self.label.config(text=f"{pred}")
-
-if __name__ == "__main__":
-  model = BigConvNet()
-  model.load("examples/checkpoint968900.safetensors")
-
-  np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-  master = tk.Tk()
-  DrawingApp(master, model)
-  master.mainloop()
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'): break
+    if key == ord('c'): input_canvas = create_blank_canvas()
+finally:
+  cv2.destroyAllWindows()
